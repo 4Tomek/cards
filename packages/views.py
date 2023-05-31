b1c091c35f362ea308881c59ef5aed2c2e0c71bc
4Tomek <zelenka@mail.muni.cz>
2023-05-31 22:43:43 +0200

    queried textbooks, sent to templates

--- packages/views.py
+++ packages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Textbook
 
 
 def packages(request):
-    page = 'packeges'
-    number = 10
-    context = {'page': page, 'number': number}
+    textbooks = Textbook.objects.all()
+    context = {'textbooks': textbooks}
     return render(request, 'packages/packages.html', context)
 
 
 def package(request, pk):
-    return render(request, 'packages/single-package.html')
+    textbookObj = Textbook.objects.get(id=pk)
+    cards = textbookObj.basic_cards_set.all()
+    context = {'textbook': textbookObj, 'cards': cards}
+    return render(request, 'packages/single-package.html', context)
