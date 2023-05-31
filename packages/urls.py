f4c93e6c1c06cb271c628722265a2a8610bed9df
4Tomek <zelenka@mail.muni.cz>
2023-05-31 19:51:09 +0200

    change packages url to 'root'

--- packages/urls.py
+++ packages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('packages/', views.packages, name="packages"),
+    path('', views.packages, name="packages"),
     path('package/<str:pk>', views.package, name="package"),
 ]
