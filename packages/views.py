05a6dd9cf1323d59d2e8c90510d9c0614a48d83c
4Tomek <zelenka@mail.muni.cz>
2023-06-01 17:57:21 +0200

    added form field to load all cards to textbook at once

--- packages/views.py
+++ packages/views.py
@@ -21,9 +21,22 @@ def createTextbook(request):
     form = PackageForm()
 
     if request.method == 'POST':
+
         form = PackageForm(request.POST)
         if form.is_valid():
-            form.save()
+            content = request.POST.get('content').split(',')
+
+            formData = form.save(commit=False)
+            textbookObj = formData.textbook
+
+            for task in content:
+                question, answer = task.split('-')
+                question = question.strip()
+                answer = answer.strip()
+                card, created = Basic_card.objects.get_or_create(
+                    question=question, answer=answer, textbook=textbookObj)
+                card.save()
+
             return redirect('packages')
 
     context = {'form': form}
