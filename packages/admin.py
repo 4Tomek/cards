e4eb43d535ecdd21282362b606d18e53691890d7
4Tomek <zelenka@mail.muni.cz>
2023-06-08 22:01:10 +0200

    added Profile, ProfileCard models

--- packages/admin.py
+++ packages/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Basic_card, Textbook, Lesson
+from .models import Textbook, Lesson, Basic_card, ProfileCard
 
-admin.site.register(Basic_card)
 admin.site.register(Textbook)
 admin.site.register(Lesson)
+admin.site.register(Basic_card)
+admin.site.register(ProfileCard)
--- packages/models.py
+++ packages/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 import uuid
+from users.models import Profile
 
 
 class Textbook(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     is_public = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
@@ -37,3 +40,20 @@ class Basic_card(models.Model):
 
     def __str__(self):
         return self.question
+
+
+class ProfileCard(models.Model):
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    card = models.ForeignKey(
+        Basic_card, on_delete=models.CASCADE, null=True, blank=True)
+    mastered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['profile', 'card']]
+
+    def __str__(self):
+        return str(self.card.question)
