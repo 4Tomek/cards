7fbd910c1bb1e5d249a6e26424c42c070f8193a2
4Tomek <zelenka@mail.muni.cz>
2023-07-03 22:47:14 +0200

    added email to change forgotten password

--- cards/settings.py
+++ cards/settings.py
@@ -117,6 +117,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Connected Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'zelenkatomas245@gmail.com'
+EMAIL_HOST_PASSWORD = 'pvmazgxgrgmpgzvl'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 from django.contrib.auth.models import User
 from .models import Profile
@@ -43,3 +45,25 @@ def updateUser(sender, instance, created, **kwargs):
 
 
 post_save.connect(updateUser, sender=Profile)
+
+
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+        subject = 'Welcome to Flashcards by Tomek'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
